fa860968f076030f0933a154b513a6625a5d7600
Sagar Joshi <joshi.sagar92@outlook.com>
2019-07-26 03:11:10 -0400

    Step3&4 : Support different delimeters

--- src/main/java/com/kata/Calculator.java
+++ src/main/java/com/kata/Calculator.java
@@ -1,15 +1,39 @@
 package com.kata;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
-    public int Add(String numbers) {
+
+    private String numbers;
+    private String regex = "[\\n|,]";
+    private static Pattern pattern = Pattern.compile("\\/\\/(;)\\\\n(.*)");
+
+    public Calculator(String numbers) {
+        this.numbers = numbers;
+        if(this.numbers.startsWith("//")) {
+            setDelimiterAndNumbers();
+        }
+    }
+
+    public int Add() {
         if("".equals(numbers)){
             return 0;
         }
-        String [] number = numbers.split(",");
+        String [] number = this.numbers.split(this.regex);
         int sum = 0;
         for(int i=0; i<number.length; i++){
             sum += Integer.parseInt(number[i]);
         }
         return sum;
     }
+
+    private void setDelimiterAndNumbers() {
+            Matcher matcher = pattern.matcher(this.numbers);
+            if(matcher.matches())
+            {
+                this.regex = matcher.group(1);
+                this.numbers = matcher.group(2);
+            }
+        }
 }
--- src/test/groovy/CalculatorSpec.groovy
+++ src/test/groovy/CalculatorSpec.groovy
@@ -7,9 +7,9 @@ class CalculatorSpec extends Specification {
     def "API to test calculator ADD method"(){
 
         given : "A String containing comma separated numbers"
-                Calculator calculator = new Calculator()
+                Calculator calculator = new Calculator(numbers)
         when  : "Add method of calculator class is called"
-                int sum = calculator.Add(numbers)
+                int sum = calculator.Add()
         then  : "It will return their sum"
                 sum == result
         where :
@@ -19,6 +19,8 @@ class CalculatorSpec extends Specification {
                 ""          ||   0
                 "5,7"       ||   12
                 "11,5,7"    ||   23
+                "1\n2,3"    ||   6
+                "//;\\n1;2" ||   3
     }
 
 }
